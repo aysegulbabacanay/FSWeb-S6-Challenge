10340287a8594d6414db6fc3fca05925a0fe5d0d
Aysegul <aysglbabacan@gmail.com>
2022-12-06 13:59:09 +0300

    ödev

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Karakter from './components/Karakter';
+import axios from "axios"
+
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  const [chars, setChars] = useState(null);
+  const [visibleIndex, setVisibleIndex] = useState(2);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/")
+      .then((res) => {
+        setChars(res.data)
+        //console.log(res.data)
+      });
+  }, [])
+  //console.log(`Karakterler: ${chars}`) karakter önce null geliyor,bu yüzden koşul koyuyoruz.
+  
 
   return (
+   
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {
+        chars === null
+        ? <div>Yükleniyor</div>
+        : <div className='chars-list'>
+          {chars.map((k ,sira) => <Karakter kisi = {k} kisiIndex={sira} acikMi={sira === visibleIndex} tiklayincaYap = {() => setVisibleIndex(sira)}/>)}
+        </div>
+      }
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,37 @@
 // Karakter bileşeniniz buraya gelecek
+import React from "react";
+import { useState } from "react";
+
+
+const Karakter = (prop) => {
+    //const {kisi}= prop;
+
+
+    const [acik, setAcik] = useState(false)
+
+
+
+    //return kısmı app.js den getirdik.
+
+    return (
+        <div onClick={prop.tiklayincaYap} className='char' key={prop.kisi.url}>
+        <h2>{prop.kisi.name}</h2> 
+        {prop.acikMi && (
+            <div className='char-ozellikleri'>
+            <div>Göz rengi: {prop.kisi.eye_color}</div>
+            <div>Boyu: {prop.kisi.height}</div>
+            <div>Kilosu: {prop.kisi.mass}</div>
+            <div>Doğum tarihi: {prop.kisi.birth_year}</div>
+          </div>
+        )}
+         
+         </div>
+    )
+}
+
+export default Karakter;
+
+
+
+
+
